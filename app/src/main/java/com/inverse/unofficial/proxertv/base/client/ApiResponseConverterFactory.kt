efde834f05a03e5e0093b9aed3c1042b4960e16e
inv3rse <3sr3vn1@gmail.com>
2016-07-20 23:10:28 +0200

    Prepare api integration

---
+++ app/src/main/java/com/inverse/unofficial/proxertv/base/client/ApiResponseConverterFactory.kt
@@ -0,0 +1,50 @@
+package com.inverse.unofficial.proxertv.base.client
+
+import okhttp3.ResponseBody
+import retrofit2.Converter
+import retrofit2.Retrofit
+import java.io.IOException
+import java.lang.reflect.Type
+
+/**
+ * Converter to simplify working with the api.
+ * The api contains nested data with a custom response code.
+ * We only return the json data field containing the actual response object or throw an exception
+ */
+class ApiResponseConverterFactory : Converter.Factory() {
+
+    override fun responseBodyConverter(type: Type, annotations: Array<Annotation>, retrofit: Retrofit): Converter<ResponseBody, *>? {
+        if (annotations.none({ annotation -> annotation is WrappedResponse })) {
+            // we do not handle this type
+            return null
+        }
+
+        val wrappedType = Types.newParameterizedType(WrappedData::class.java, type)
+
+        // delegate actual parsing to next converter (gson)
+        val delegate = retrofit.nextResponseBodyConverter<WrappedData<*>>(this, wrappedType, annotations)
+        return ApiResponseConverter(delegate)
+    }
+
+    class ApiResponseConverter<T>(val delegate: Converter<ResponseBody, WrappedData<T>>) : Converter<ResponseBody, T> {
+
+        override fun convert(value: ResponseBody): T {
+
+            val wrappedData = delegate.convert(value)
+            if (wrappedData.data == null) {
+                throw IOException(wrappedData.message)
+            }
+
+            return wrappedData.data
+        }
+    }
+}
+
+annotation class WrappedResponse
+
+data class WrappedData<out T>(
+        val error: Int,
+        val message: String,
+        val data: T?,
+        val code: Int?
+)
---
+++ app/src/main/java/com/inverse/unofficial/proxertv/base/client/Types.java
@@ -0,0 +1,282 @@
+package com.inverse.unofficial.proxertv.base.client;
+
+/*
+ * Copyright (C) 2008 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.lang.reflect.GenericArrayType;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.lang.reflect.TypeVariable;
+import java.lang.reflect.WildcardType;
+import java.util.Arrays;
+
+/**
+ * Factory methods for types.
+ */
+final class Types {
+    static final Type[] EMPTY_TYPE_ARRAY = new Type[]{};
+
+    private Types() {
+    }
+
+    /**
+     * Returns a new parameterized type, applying {@code typeArguments} to {@code rawType}.
+     */
+    public static ParameterizedType newParameterizedType(Type rawType, Type... typeArguments) {
+        return new ParameterizedTypeImpl(null, rawType, typeArguments);
+    }
+
+    /**
+     * Returns a type that is functionally equal but not necessarily equal according to {@link
+     * Object#equals(Object) Object.equals()}.
+     */
+    static Type canonicalize(Type type) {
+        if (type instanceof Class) {
+            Class<?> c = (Class<?>) type;
+            return c.isArray() ? new GenericArrayTypeImpl(canonicalize(c.getComponentType())) : c;
+        } else if (type instanceof ParameterizedType) {
+            ParameterizedType p = (ParameterizedType) type;
+            return new ParameterizedTypeImpl(p.getOwnerType(), p.getRawType(), p.getActualTypeArguments());
+        } else if (type instanceof GenericArrayType) {
+            GenericArrayType g = (GenericArrayType) type;
+            return new GenericArrayTypeImpl(g.getGenericComponentType());
+        } else if (type instanceof WildcardType) {
+            WildcardType w = (WildcardType) type;
+            return new WildcardTypeImpl(w.getUpperBounds(), w.getLowerBounds());
+        } else {
+            return type; // This type is unsupported!
+        }
+    }
+
+    static boolean equal(Object a, Object b) {
+        return a == b || (a != null && a.equals(b));
+    }
+
+    /**
+     * Returns true if {@code a} and {@code b} are equal.
+     */
+    static boolean equals(Type a, Type b) {
+        if (a == b) {
+            return true; // Also handles (a == null && b == null).
+        } else if (a instanceof Class) {
+            return a.equals(b); // Class already specifies equals().
+        } else if (a instanceof ParameterizedType) {
+            if (!(b instanceof ParameterizedType))
+                return false;
+            ParameterizedType pa = (ParameterizedType) a;
+            ParameterizedType pb = (ParameterizedType) b;
+            Type[] aTypeArguments = pa instanceof ParameterizedTypeImpl ? ((ParameterizedTypeImpl) pa).typeArguments : pa
+                    .getActualTypeArguments();
+            Type[] bTypeArguments = pb instanceof ParameterizedTypeImpl ? ((ParameterizedTypeImpl) pb).typeArguments : pb
+                    .getActualTypeArguments();
+            return equal(pa.getOwnerType(), pb.getOwnerType()) && pa.getRawType()
+                    .equals(pb.getRawType()) && Arrays.equals(aTypeArguments, bTypeArguments);
+        } else if (a instanceof GenericArrayType) {
+            if (!(b instanceof GenericArrayType))
+                return false;
+            GenericArrayType ga = (GenericArrayType) a;
+            GenericArrayType gb = (GenericArrayType) b;
+            return equals(ga.getGenericComponentType(), gb.getGenericComponentType());
+        } else if (a instanceof WildcardType) {
+            if (!(b instanceof WildcardType))
+                return false;
+            WildcardType wa = (WildcardType) a;
+            WildcardType wb = (WildcardType) b;
+            return Arrays.equals(wa.getUpperBounds(), wb.getUpperBounds()) && Arrays.equals(wa.getLowerBounds(), wb
+                    .getLowerBounds());
+        } else if (a instanceof TypeVariable) {
+            if (!(b instanceof TypeVariable))
+                return false;
+            TypeVariable<?> va = (TypeVariable<?>) a;
+            TypeVariable<?> vb = (TypeVariable<?>) b;
+            return va.getGenericDeclaration() == vb.getGenericDeclaration() && va.getName()
+                    .equals(vb.getName());
+        } else {
+            // This isn't a supported type. Could be a generic array type, wildcard type, etc.
+            return false;
+        }
+    }
+
+    static int hashCodeOrZero(Object o) {
+        return o != null ? o.hashCode() : 0;
+    }
+
+    static String typeToString(Type type) {
+        return type instanceof Class ? ((Class<?>) type).getName() : type.toString();
+    }
+
+    static void checkNotPrimitive(Type type) {
+        if ((type instanceof Class<?>) && ((Class<?>) type).isPrimitive()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static final class ParameterizedTypeImpl implements ParameterizedType {
+        final Type[] typeArguments;
+        private final Type ownerType;
+        private final Type rawType;
+
+        ParameterizedTypeImpl(Type ownerType, Type rawType, Type... typeArguments) {
+            // require an owner type if the raw type needs it
+            if (rawType instanceof Class<?>) {
+                Class<?> rawTypeAsClass = (Class<?>) rawType;
+                boolean isStaticOrTopLevelClass = Modifier.isStatic(rawTypeAsClass.getModifiers()) || rawTypeAsClass
+                        .getEnclosingClass() == null;
+                if (ownerType == null && !isStaticOrTopLevelClass)
+                    throw new IllegalArgumentException();
+            }
+
+            this.ownerType = ownerType == null ? null : canonicalize(ownerType);
+            this.rawType = canonicalize(rawType);
+            this.typeArguments = typeArguments.clone();
+            for (int t = 0; t < this.typeArguments.length; t++) {
+                if (this.typeArguments[t] == null)
+                    throw new NullPointerException();
+                checkNotPrimitive(this.typeArguments[t]);
+                this.typeArguments[t] = canonicalize(this.typeArguments[t]);
+            }
+        }
+
+        public Type[] getActualTypeArguments() {
+            return typeArguments.clone();
+        }
+
+        public Type getRawType() {
+            return rawType;
+        }
+
+        public Type getOwnerType() {
+            return ownerType;
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            return other instanceof ParameterizedType && Types.equals(this, (ParameterizedType) other);
+        }
+
+        @Override
+        public int hashCode() {
+            return Arrays.hashCode(typeArguments) ^ rawType.hashCode() ^ hashCodeOrZero(ownerType);
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder result = new StringBuilder(30 * (typeArguments.length + 1));
+            result.append(typeToString(rawType));
+
+            if (typeArguments.length == 0) {
+                return result.toString();
+            }
+
+            result.append("<").append(typeToString(typeArguments[0]));
+            for (int i = 1; i < typeArguments.length; i++) {
+                result.append(", ").append(typeToString(typeArguments[i]));
+            }
+            return result.append(">").toString();
+        }
+    }
+
+    private static final class GenericArrayTypeImpl implements GenericArrayType {
+        private final Type componentType;
+
+        public GenericArrayTypeImpl(Type componentType) {
+            this.componentType = canonicalize(componentType);
+        }
+
+        public Type getGenericComponentType() {
+            return componentType;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            return o instanceof GenericArrayType && Types.equals(this, (GenericArrayType) o);
+        }
+
+        @Override
+        public int hashCode() {
+            return componentType.hashCode();
+        }
+
+        @Override
+        public String toString() {
+            return typeToString(componentType) + "[]";
+        }
+    }
+
+    /**
+     * The WildcardType interface supports multiple upper bounds and multiple lower bounds. We only
+     * support what the Java 6 language needs - at most one bound. If a lower bound is set, the upper
+     * bound must be Object.class.
+     */
+    private static final class WildcardTypeImpl implements WildcardType {
+        private final Type upperBound;
+        private final Type lowerBound;
+
+        public WildcardTypeImpl(Type[] upperBounds, Type[] lowerBounds) {
+            if (lowerBounds.length > 1)
+                throw new IllegalArgumentException();
+            if (upperBounds.length != 1)
+                throw new IllegalArgumentException();
+
+            if (lowerBounds.length == 1) {
+                if (lowerBounds[0] == null)
+                    throw new NullPointerException();
+                checkNotPrimitive(lowerBounds[0]);
+                if (upperBounds[0] != Object.class)
+                    throw new IllegalArgumentException();
+                this.lowerBound = canonicalize(lowerBounds[0]);
+                this.upperBound = Object.class;
+            } else {
+                if (upperBounds[0] == null)
+                    throw new NullPointerException();
+                checkNotPrimitive(upperBounds[0]);
+                this.lowerBound = null;
+                this.upperBound = canonicalize(upperBounds[0]);
+            }
+        }
+
+        public Type[] getUpperBounds() {
+            return new Type[]{upperBound};
+        }
+
+        public Type[] getLowerBounds() {
+            return lowerBound != null ? new Type[]{lowerBound} : EMPTY_TYPE_ARRAY;
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            return other instanceof WildcardType && Types.equals(this, (WildcardType) other);
+        }
+
+        @Override
+        public int hashCode() {
+            // This equals Arrays.hashCode(getLowerBounds()) ^ Arrays.hashCode(getUpperBounds()).
+            return (lowerBound != null ? 31 + lowerBound.hashCode() : 1) ^ (31 + upperBound.hashCode());
+        }
+
+        @Override
+        public String toString() {
+            if (lowerBound != null) {
+                return "? super " + typeToString(lowerBound);
+            } else if (upperBound == Object.class) {
+                return "?";
+            } else {
+                return "? extends " + typeToString(upperBound);
+            }
+        }
+    }
+}
