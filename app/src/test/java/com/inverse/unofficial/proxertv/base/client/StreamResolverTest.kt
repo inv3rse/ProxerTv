a2473f43161785071d513c2c7ae63664187545e4
inv3rse <3sr3vn1@gmail.com>
2019-02-07 17:04:34 +0100

    Fix lint & tests

--- app/src/test/java/com/inverse/unofficial/proxertv/base/client/StreamResolverTest.kt
+++ app/src/test/java/com/inverse/unofficial/proxertv/base/client/StreamResolverTest.kt
@@ -13,6 +13,7 @@ import org.junit.After
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
+import rx.Observable
 import subscribeAssert
 
 class StreamResolverTest {
@@ -24,8 +25,8 @@ class StreamResolverTest {
         mockServer.start()
 
         httpClient = OkHttpClient.Builder()
-                .addInterceptor(MockRedirector(mockServer.url("").toString()))
-                .build()
+            .addInterceptor(MockRedirector(mockServer.url("").toString()))
+            .build()
     }
 
     @After
@@ -42,10 +43,10 @@ class StreamResolverTest {
         assertTrue(resolver.appliesToUrl("https://stream.proxer.me/embed-zbtli7pqg8mg-728x504.html"))
 
         resolver.resolveStream("http://stream.proxer.me/embed-zbtli7pqg8mg-728x504.html")
-                .subscribeAssert {
-                    assertNoErrors()
-                    assertValue(Stream("http://s36.stream.proxer.me/files/7/wqj6extx5r6ztv/video.mp4", ""))
-                }
+            .subscribeAssert {
+                assertNoErrors()
+                assertValue(Stream("http://s36.stream.proxer.me/files/7/wqj6extx5r6ztv/video.mp4", ""))
+            }
     }
 
     @Test
@@ -58,10 +59,15 @@ class StreamResolverTest {
         assertTrue(resolver.appliesToUrl("https://streamcloud.eu/rma6ijnb58n0"))
 
         resolver.resolveStream("http://streamcloud.eu/rma6ijnb58n0")
-                .subscribeAssert {
-                    assertNoErrors()
-                    assertValue(Stream("http://cdn8.streamcloud.eu:8080/zpv75jl4lwoax3ptx32ihr7fuses3udb2dpgvrl344izxpszn4p4ldp3nm/video.mp4", ""))
-                }
+            .subscribeAssert {
+                assertNoErrors()
+                assertValue(
+                    Stream(
+                        "http://cdn8.streamcloud.eu:8080/zpv75jl4lwoax3ptx32ihr7fuses3udb2dpgvrl344izxpszn4p4ldp3nm/video.mp4",
+                        ""
+                    )
+                )
+            }
     }
 
     @Test
@@ -74,7 +80,12 @@ class StreamResolverTest {
 
         resolver.resolveStream("http://www.mp4upload.com/embed-sg71lpccevaa.html").subscribeAssert {
             assertNoErrors()
-            assertValue(Stream("https://www2.mp4upload.com:282/d/rgxzcgtmz3b4quuofwvbwksoi244w36p4ot4zdpu3sak6gbf5myfbrny/video.mp4", ""))
+            assertValue(
+                Stream(
+                    "https://www2.mp4upload.com:282/d/rgxzcgtmz3b4quuofwvbwksoi244w36p4ot4zdpu3sak6gbf5myfbrny/video.mp4",
+                    ""
+                )
+            )
         }
     }
 
@@ -87,15 +98,23 @@ class StreamResolverTest {
         assertTrue(resolver.appliesToUrl("https://www.dailymotion.com/embed/video/x5tqens"))
 
         resolver.resolveStream("http://www.dailymotion.com/embed/video/x5tqens")
-                .subscribeAssert {
-                    assertNoErrors()
-                    assertValues(
-                            Stream("http://www.dailymotion.com/cdn/H264-1280x720/video/x5tqens.mp4?auth=1501533278-2688-ysrzfgvg-81963c69aacf70e54cc2fa56f209af40", ""),
-                            Stream("http://www.dailymotion.com/cdn/H264-848x480/video/x5tqens.mp4?auth=1501533278-2688-r8o5lwqa-ab206b484b0ab0de4c77255027008251", ""))
-                }
+            .subscribeAssert {
+                assertNoErrors()
+                assertValues(
+                    Stream(
+                        "http://www.dailymotion.com/cdn/H264-1280x720/video/x5tqens.mp4?auth=1501533278-2688-ysrzfgvg-81963c69aacf70e54cc2fa56f209af40",
+                        ""
+                    ),
+                    Stream(
+                        "http://www.dailymotion.com/cdn/H264-848x480/video/x5tqens.mp4?auth=1501533278-2688-r8o5lwqa-ab206b484b0ab0de4c77255027008251",
+                        ""
+                    )
+                )
+            }
     }
 
-    private fun StreamResolver.appliesToUrl(url: String) = appliesToUrl(HttpUrl.parse(url))
+    private fun StreamResolver.appliesToUrl(url: String) = HttpUrl.parse(url)?.let { appliesToUrl(it) } ?: false
 
-    private fun StreamResolver.resolveStream(url: String) = resolveStream(HttpUrl.parse(url))
+    private fun StreamResolver.resolveStream(url: String) =
+        HttpUrl.parse(url)?.let { resolveStream(it) } ?: Observable.empty()
 }
